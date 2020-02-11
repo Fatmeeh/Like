1668faf1a00de93769dfa5a45356bd5ec3068c7e
MahmodMhamed <mahmod.mhamid@hotmail.com>
2020-02-11 14:45:20 +0200

    add style file

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,21 @@ const express = require('express');
 const userListFromDatabase = require('./../model');
 
 const router = express.Router();
+
 router.get('/', (req, res) => {
-  res.render('home', { users: userListFromDatabase });
+  res.render('home');
+});
+
+router.get('/cars', (req, res) => {
+  res.render('home', { data: 'This is cars' });
+});
+
+router.get('/flowers', (req, res) => {
+  res.render('home', { data: 'This is flowers' });
+});
+
+router.get('/animals', (req, res) => {
+  res.render('home', { data: 'This is animals' });
 });
 
 module.exports = router;
