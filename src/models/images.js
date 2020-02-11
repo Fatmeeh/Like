3e61a070412cacfdef96f5e55856885b30de03d0
Francis Aboud <francisaboud2000@gmail.com>
2020-02-11 17:16:05 +0200

    affef model for liked_pic table

--- src/models/images.js
+++ src/models/images.js
@@ -12,20 +12,8 @@ const getAllImages = (category,cb) => {
       cb(null, res.rows);
     });
   };
-
-  const postLiked = (img_id, user_id, cb) => {
-    dbConnection.query(
-      "insert into liked_pic(img_id,user_id) values ($1,$2)",
-      [img_id, user_id],
-      (err, res) => {
-        if (err) return cb(err);
-        cb(null, res.rows);
-      }
-    );
-  };
   
   module.exports = {
     getAllImages,
-    getCategoryImages,
-    postLiked
+    getCategoryImages
   }
