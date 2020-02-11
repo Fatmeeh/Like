67ac1e222a8a28b7c056dc28c84e4710926b3559
Fatmeeh <lasheenfatmeeh@gmail.com>
2020-02-11 16:09:06 +0200

    push

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,13 @@ const express = require('express');
 const userListFromDatabase = require('./../model');
 
 const router = express.Router();
-
 router.get('/', (req, res) => {
+  res.render('loading', { title: 'LIKE | Home' });
+});
+router.get('/logIn', (req, res) => {
+  res.render('logIn', { title: 'LIKE | Home' });
+});
+router.get('/home', (req, res) => {
   res.render('home', { title: 'LIKE | Home' });
 });
 
