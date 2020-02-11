ce17ffcf83396cbfcbab1a34f540ccbe95ed4090
Francis Aboud <francisaboud2000@gmail.com>
2020-02-11 16:59:27 +0200

    added users status

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -20,14 +20,15 @@ CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY,
     user_name VARCHAR(255) NOT NULL,
     user_pass VARCHAR(255) NOT NULL,
-    user_img VARCHAR(255) NOT NULL
+    user_img VARCHAR(255) NOT NULL,
+    user_status VARCHAR(255) NOT NULL
 );
 
 INSERT INTO users(user_name,user_pass,user_img) 
 VALUES
-('francis','123Francis','https://i.ytimg.com/vi/JlxU9mAPnMg/hqdefault.jpg'),
-('fatmeeh','123Fatmeeh','https://i.ytimg.com/vi/JlxU9mAPnMg/hqdefault.jpg'),
-('mahmod','123Mahmod','https://i.ytimg.com/vi/JlxU9mAPnMg/hqdefault.jpg');
+('francis','123Francis','https://i.ytimg.com/vi/JlxU9mAPnMg/hqdefault.jpg','Travel to life is like css to html'),
+('fatmeeh','123Fatmeeh','https://i.ytimg.com/vi/JlxU9mAPnMg/hqdefault.jpg','HTTP 404: Status not Found'),
+('mahmod','123Mahmod','https://i.ytimg.com/vi/JlxU9mAPnMg/hqdefault.jpg','Do not be afraid to step on people. Mario made a career from it');
 
 CREATE TABLE IF NOT EXISTS liked_pic (
     like_id SERIAL PRIMARY KEY,
