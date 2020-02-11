c604367dc8bb26386edce4689ca5ad5ea86857b8
Francis Aboud <francisaboud2000@gmail.com>
2020-02-11 13:46:55 +0200

    added protection and logs collection

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const path = require('path');
-
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const compression = require("compression");
+const fs = require("fs");
+const path = require("path");
+const helmet = require("helmet");
+const exphbs = require("express-handlebars")
 const routes = require('./routes/index');
 const helpers = require('./views/helpers/index');
 
 const app = express();
+
+app.use(helmet()); // protect the f**king server
+app.use(cors()); // will serve data for any kind of client-side applications
+app.use(compression()); //make requests lighter and load faster
+
+// create a write stream in append mode
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "..", "logs", "access.log"),
+  { flag: "a" }
+);
+// setup morgan logger in 'combined' and stream data to the write stream
+app.use(morgan("combined", { stream: accessLogStream }));
+
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
