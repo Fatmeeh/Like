d7085a85e6f7dc8d99873bb7deba213a24d7338a
MahmodMhamed <mahmod.mhamid@hotmail.com>
2020-02-11 14:05:52 +0200

    create a home page

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const userListFromDatabase = require('./../model');
 
 const router = express.Router();
-
 router.get('/', (req, res) => {
   res.render('home', { users: userListFromDatabase });
 });
