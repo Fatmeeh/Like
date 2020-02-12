3a0bf0225f4d6b2bf9cfe30f825cb0e2e3e9fbfc
Francis Aboud <francisaboud2000@gmail.com>
2020-02-12 15:17:34 +0200

    added get users and load users for userProfilePage

--- src/models/liked_pic.js
+++ src/models/liked_pic.js
@@ -1,4 +1,12 @@
 const dbConnection = require("../database/db_connection")
+
+const getLiked = (userId,cb) => {
+    dbConnection.query("SELECT img_info,img_url,img_category FROM images JOIN liked_pic ON liked_pic.img_id = images.img_id JOIN users ON ($1) = liked_pic.user_id;",[userId], (err, res) => {
+      if (err) return cb(err);
+      cb(null, res.rows);
+    });
+  };
+
   const postLiked = (img_id, user_id, cb) => {
     dbConnection.query(
       "insert into liked_pic(img_id,user_id) values ($1,$2)",
@@ -9,7 +17,8 @@ const dbConnection = require("../database/db_connection")
       }
     );
   };
-  
+
   module.exports = {
+    getLiked,
     postLiked
   }
