ff403f914a6476fa52bfcacddd04d19346ea8006
Francis Aboud <francisaboud2000@gmail.com>
2020-02-12 19:05:36 +0200

    added the like hearts pic got to the user profile and secured the website

--- src/controllers/images.js
+++ src/controllers/images.js
@@ -3,21 +3,21 @@ const dataModel = require('../models');
 const getAnimals = (req, res) => {
   dataModel.images.getCategoryImages('Animals', (err, data) => {
     if (err) console.log(err);
-    res.render('home', { title: 'LIKE | Animals', data: data });
+    res.render('home', { title: 'LIKE | Animals', data: data,id:1 });
   });
 };
 
 const getFlowers = (req, res) => {
   dataModel.images.getCategoryImages('flowers', (err, data) => {
     if (err) console.log(err);
-    res.render('home', { title: 'LIKE | Flowers', data: data });
+    res.render('home', { title: 'LIKE | Flowers', data: data ,id:1});
   });
 };
 
 const getCars = (req, res) => {
   dataModel.images.getCategoryImages('Cars', (err, data) => {
     if (err) console.log(err);
-    res.render('home', { title: 'LIKE | Cars', data: data });
+    res.render('home', { title: 'LIKE | Cars', data: data,id:1 });
   });
 };
 
