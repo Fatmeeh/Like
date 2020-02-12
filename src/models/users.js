07f1ffd5052648aa38cc9d99edca3980b2c48b6b
Francis Aboud <francisaboud2000@gmail.com>
2020-02-12 16:46:07 +0200

    did the valedation of users in login page

--- src/models/users.js
+++ src/models/users.js
@@ -5,13 +5,20 @@ const getAllUsers = cb => {
     cb(null, res.rows);
   });
 };
-const getUserByName = (userName,cb) => {
-    dbConnection.query("SELECT * FROM users WHERE user_name = ($1) ",[userName], (err, res) => {
+const getUserById = (userId,cb) => {
+    dbConnection.query("SELECT * FROM users WHERE user_id = ($1) ",[userId], (err, res) => {
+      if (err) return cb(err);
+      cb(null, res.rows);
+    });
+  };
+  const getUserByNamePass = (userName,userPass,cb) => {
+    dbConnection.query("SELECT user_id FROM users WHERE user_name = ($1) AND user_pass = ($2)",[userName,userPass], (err, res) => {
       if (err) return cb(err);
       cb(null, res.rows);
     });
   };
  module.exports = {
     getAllUsers,
-    getUserByName
+    getUserById,
+    getUserByNamePass
  }
